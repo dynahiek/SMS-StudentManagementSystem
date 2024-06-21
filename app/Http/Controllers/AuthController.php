7ffc9903e26ec9077e8f3506f9a8894c25561571
dynahiek <hiekdina@gmail.com>
2024-06-21 23:17:19 +0700

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Mail;
 class AuthController extends Controller
 {
     public function login(){
-        //dd(Hash::make(123456));
+        // dd(Hash::make(123456));
         if(!empty(Auth::check())){
-
             if(Auth::user()->user_type == 1){
                 return redirect('admin/dashboard');
             }
@@ -31,6 +30,7 @@ class AuthController extends Controller
         }
         return view('auth.login');
     }
+    
     public function AuthLogin(Request $request){
         $remember = !empty($request->remember) ? true : false;
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password], true)){
